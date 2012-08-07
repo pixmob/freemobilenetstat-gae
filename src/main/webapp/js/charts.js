6d486edaaf1a4ce4d2f8210fb5bc58d202480fc6
Alexandre Roman <alexandre.roman@gmail.com>
2012-08-07 12:16:29 +0200

    Update index page

--- src/main/webapp/js/charts.js
+++ src/main/webapp/js/charts.js
@@ -14,6 +14,9 @@ function drawChart(jsonData) {
     users = jsonData["users"];
     $("#users").append(users);
 
+    days = jsonData["days"];
+    $("#days").append(days);
+
     onOrange = jsonData["orange"];
     onFreeMobile = jsonData["freeMobile"];
 
